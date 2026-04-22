b5d1b79187defdf64ca1c6e4b405eeb1c7e87923
malshapraboth <malshapraboth@gmail.com>
2026-04-22 22:34:33 +0200

    more style

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/components/Home.vue';
 import Business from '../components/Business.vue';
+import About from '../components/About.vue';
 
 
 const routes = [
@@ -8,6 +9,11 @@ const routes = [
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
     },
     {
         path: '/business',
