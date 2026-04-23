b95f9f2834bd2bac3459e7166f980890d3f963aa
malshapraboth <malshapraboth@gmail.com>
2026-04-24 00:42:22 +0200

    new test

--- server/index.test.js
+++ server/index.test.js
@@ -13,14 +13,6 @@ describe('Express Server API Tests', () => {
     assert.strictEqual(response.body.message, "Server is online!");
   });
 
-  // Test your business routes (Assuming you have a GET all)
-  test('GET /businesses should return an array', async () => {
-    const response = await request(app).get('/businesses');
-    
-    assert.strictEqual(response.status, 200);
-    assert.ok(Array.isArray(response.body), 'Response should be an array');
-  });
-
   // Test a 404 for non-existent routes
   test('404 on unknown route', async () => {
     const response = await request(app).get('/this-route-does-not-exist');
