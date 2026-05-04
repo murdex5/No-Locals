8628da72d63746eaecfe1001ecff240ad5ebc410
malshapraboth <malshapraboth@gmail.com>
2026-05-04 02:15:09 +0200

    new tests

--- server/index.test.js
+++ server/index.test.js
@@ -1,20 +1,23 @@
 import { test, describe } from 'node:test';
 import assert from 'node:assert';
 import request from 'supertest';
-import app from './index.js'; 
+import app from './index.js';
 
 describe('Express Server API Tests', () => {
 
   test('GET /test returns correct message', async () => {
-    const response = await request(app).get('/test');
-    
+    const response = await request(app)
+      .get('/test')
+      .set('Accept', 'application/json'); 
     assert.strictEqual(response.status, 200);
-    // Changed to match your server's actual output
-    assert.strictEqual(response.body.message, "Test route is working"); 
+    assert.strictEqual(response.body.message, "Test route is working");
   });
 
-  test('404 on unknown route', async () => {
-    const response = await request(app).get('/this-route-does-not-exist');
+  test('404 on unknown API route', async () => {
+    const response = await request(app)
+      .get('/this-api-route-does-not-exist')
+      .set('Accept', 'application/json'); 
+
     assert.strictEqual(response.status, 404);
   });
 });
