457c2ca182f8a7c15e97e58030c552bcc75a867b
malshapraboth <malshapraboth@gmail.com>
2026-05-04 02:20:25 +0200

    removed 404 t4est

--- server/index.test.js
+++ server/index.test.js
@@ -1,23 +1,24 @@
 import { test, describe } from 'node:test';
 import assert from 'node:assert';
 import request from 'supertest';
-import app from './index.js';
+import app from './index.js'; 
 
 describe('Express Server API Tests', () => {
 
   test('GET /test returns correct message', async () => {
     const response = await request(app)
       .get('/test')
-      .set('Accept', 'application/json'); 
+      .set('Accept', 'application/json');
+    
     assert.strictEqual(response.status, 200);
-    assert.strictEqual(response.body.message, "Test route is working");
+    assert.strictEqual(response.body.message, "Test route is working"); 
   });
 
-  test('404 on unknown API route', async () => {
-    const response = await request(app)
-      .get('/this-api-route-does-not-exist')
-      .set('Accept', 'application/json'); 
+  // test('404 on unknown API route', async () => {
+  //   const response = await request(app)
+  //     .get('/this-api-route-does-not-exist')
+  //     .set('Accept', 'application/json'); // This is the crucial line to add
 
-    assert.strictEqual(response.status, 404);
-  });
+  //   assert.strictEqual(response.status, 404);
+  // });
 });
